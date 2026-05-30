38c9d571de18e2fde00f0e9f543d64f278696b7d
KevinNunez-bit <loleirokev@gmail.com>
2026-05-29 17:32:19 -0700

    Clase crucigrama y dominios

--- crucigramas.py
+++ crucigramas.py
@@ -3,24 +3,160 @@ import time
 
 
 class Crucigrama(csps.ProblemaCSP):
-    def __init__(self, pos_ini):
-        self.X = # TODO: definir el conjunto de variables
-        self.D = # TODO: definir el dominio de cada variable
-        self.N = # TODO: definir el conjunto de vecinos de cada variable
-        
+    """
+    CSP para construir un crucigrama a partir de dos listas de palabras.
+
+    Variables: cada palabra identificada por (direccion, texto)
+    Dominio: posibles posiciones (fila, columna) de inicio en la cuadricula
+    Restricciones: H y V deben cruzarse con letra coincidente,
+                   palabras del mismo tipo no pueden traslaparse ni pegarse
+    """
+
+    def __init__(self, horizontales, verticales, n, m):
+        self.horizontales = horizontales
+        self.verticales = verticales
+        self.n = n
+        self.m = m
+
+        self.X = set()
+        for w in horizontales:
+            self.X.add(('H', w))
+        for w in verticales:
+            self.X.add(('V', w))
+
+        self.D = {}
+        for w in horizontales:
+            self.D[('H', w)] = {
+                (r, c)
+                for r in range(n)
+                for c in range(m - len(w) + 1)
+            }
+        for w in verticales:
+            self.D[('V', w)] = {
+                (r, c)
+                for r in range(n - len(w) + 1)
+                for c in range(m)
+            }
+
+        self._reducir_dominios()
+        self.N = {var: self.X - {var} for var in self.X}
+
+    def _celdas(self, var, pos):
+        dir_, word = var
+        r, c = pos
+        if dir_ == 'H':
+            return {(r, c + i): word[i] for i in range(len(word))}
+        else:
+            return {(r + i, c): word[i] for i in range(len(word))}
+
+    def _reducir_dominios(self):
+        vars_h = [v for v in self.X if v[0] == 'H']
+        vars_v = [v for v in self.X if v[0] == 'V']
+
+        cambio = True
+        while cambio:
+            cambio = False
+            for var_h in vars_h:
+                antes = len(self.D[var_h])
+                validas = set()
+                for pos_h in self.D[var_h]:
+                    celdas_h = self._celdas(var_h, pos_h)
+                    puede_cruzar_todos = True
+                    for var_v in vars_v:
+                        puede = False
+                        for pos_v in self.D[var_v]:
+                            celdas_v = self._celdas(var_v, pos_v)
+                            for celda in set(celdas_h) & set(celdas_v):
+                                if celdas_h[celda] == celdas_v[celda]:
+                                    puede = True
+                                    break
+                            if puede:
+                                break
+                        if not puede:
+                            puede_cruzar_todos = False
+                            break
+                    if puede_cruzar_todos:
+                        validas.add(pos_h)
+                self.D[var_h] = validas
+                if len(validas) != antes:
+                    cambio = True
+
+            for var_v in vars_v:
+                antes = len(self.D[var_v])
+                validas = set()
+                for pos_v in self.D[var_v]:
+                    celdas_v = self._celdas(var_v, pos_v)
+                    puede_cruzar_todos = True
+                    for var_h in vars_h:
+                        puede = False
+                        for pos_h in self.D[var_h]:
+                            celdas_h = self._celdas(var_h, pos_h)
+                            for celda in set(celdas_h) & set(celdas_v):
+                                if celdas_h[celda] == celdas_v[celda]:
+                                    puede = True
+                                    break
+                            if puede:
+                                break
+                        if not puede:
+                            puede_cruzar_todos = False
+                            break
+                    if puede_cruzar_todos:
+                        validas.add(pos_v)
+                self.D[var_v] = validas
+                if len(validas) != antes:
+                    cambio = True
 
     def restriccion_binaria(self, xi, vi, xj, vj):
-        # TODO: definir la función de restricción binaria entre las variables xi y xj
-        pass
-    
-def prueba_crucigrama(verticales, horizontales, consistencia=1):
-    
-    # TODO: Probar el CSP del crucigrama con el grafo de restricciones con consistencia dada y medir el tiempo que tarda en resolverlo. Imprimir la asignación resultante, el número de backtrackings realizados y el tiempo que tardó en resolverlo.
-    
-    raise NotImplementedError("Completa la función prueba_crucigrama para probar tu implementación del CSP del crucigrama")
+        dir_i, word_i = xi
+        dir_j, word_j = xj
+        ri, ci = vi
+        rj, cj = vj
+
+        celdas_i = self._celdas(xi, vi)
+        celdas_j = self._celdas(xj, vj)
+        comunes = set(celdas_i.keys()) & set(celdas_j.keys())
+
+        if dir_i == dir_j == 'H':
+            if comunes:
+                return False
+            if ri == rj:
+                fin_i = ci + len(word_i)
+                fin_j = cj + len(word_j)
+                if fin_i == cj or fin_j == ci:
+                    return False
+            if abs(ri - rj) == 1:
+                fin_i = ci + len(word_i) - 1
+                fin_j = cj + len(word_j) - 1
+                if ci <= fin_j and cj <= fin_i:
+                    return False
+
+        elif dir_i == dir_j == 'V':
+            if comunes:
+                return False
+            if ci == cj:
+                fin_i = ri + len(word_i)
+                fin_j = rj + len(word_j)
+                if fin_i == rj or fin_j == ri:
+                    return False
+            if abs(ci - cj) == 1:
+                fin_i = ri + len(word_i) - 1
+                fin_j = rj + len(word_j) - 1
+                if ri <= fin_j and rj <= fin_i:
+                    return False
+
+        else:
+            if not comunes:
+                return False
+            for celda in comunes:
+                if celdas_i[celda] != celdas_j[celda]:
+                    return False
+
+        return True
+
+
+def prueba_crucigrama(horizontales, verticales, n, m, consistencia=1):
+    pass
+
 
 if __name__ == "__main__":
-    
-    prueba_crucigrama(...) # TODO: definir los crucigramas a probar
-
-         
+    prueba_crucigrama([], [], 5, 5)
